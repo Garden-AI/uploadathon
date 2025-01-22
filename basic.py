65eecdfd90e56ec852bbf713610e8fb947e8a295
Will Engler <Engler.Will@gmail.com>
2025-01-22 08:18:21 -0600

    add first two examples

---
+++ basic.py
@@ -0,0 +1,14 @@
+import modal
+
+custom_image = (
+    modal.Image.debian_slim(python_version="3.11")
+    .pip_install("numpy==1.24.3")
+)
+
+app = modal.App("hello-garden")
+
+@app.function()
+def say_hello():
+    import numpy as np
+    print(np.__version__)
+    return "Hello, Garden!"
---
+++ needs_a_tweak.py
@@ -0,0 +1,16 @@
+import modal
+
+custom_image = (
+    modal.Image.debian_slim(python_version="3.11")
+)
+
+app = modal.App("hello-modal")
+
+@app.function()
+def say_something():
+    message = # oops, I forgot to write a message
+    return message
+
+@app.local_entrypoint()
+def main():
+    print(say_something.remote())
